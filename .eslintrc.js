aece462963b98c5e232435df2149f6accf575abc
Logan McAnsh <logan@mcan.sh>
2022-11-30 13:30:22 -0500

    chore: add eslint-plugin-remix-react-routes
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,16 @@ module.exports = {
     "@remix-run/eslint-config/node",
     "@remix-run/eslint-config/internal",
   ],
+  // Report unused `eslint-disable` comments.
+  reportUnusedDisableDirectives: true,
+  // Tell ESLint not to ignore dot-files, which are ignored by default.
+  ignorePatterns: ["!.*.js"],
+
+  plugins: ["remix-react-routes"],
   rules: {
     "react/jsx-no-leaked-render": ["error", { validStrategies: ["ternary"] }],
+    "remix-react-routes/require-valid-paths": "error",
+    "remix-react-routes/no-relative-paths": "error",
+    "remix-react-routes/no-urls": "error",
   },
 };
